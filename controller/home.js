5500b381ff45a967d7e2c87fa32ec0bc2de3b367
robiulhasanrohan <robiul@blogdesire.com>
2021-12-10 17:44:24 +0600

    fixing api calling

--- controller/home.js
+++ controller/home.js
@@ -2,24 +2,29 @@ const fileSchema = require('../db/file db/fileSchema')
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 module.exports = {
-    gethome: (req, res) => {
+    gethome: async (req, res) => {
         res.render('pages/home')
     },
-    posthome: (req, res, next) => {
-        let uuid = uuidv4()
-        if (req.file) {
-            const { filename, path } = req.file
-            const fileData = new fileSchema({
-                filename: filename,
-                path: path,
-                uuid: uuid
-            });
-            fileData.save(function (err, data) {
-                if (err) throw err;
-            });
-            res.json({
-                fileEndpoint: `${req.header('protocol') ? req.header('protocol') : ''}${req.header('Host')}/uploadfile/${uuid}`
-            })
+    posthome: async (req, res, next) => {
+        try {
+            let uuid = uuidv4()
+            if (req.file) {
+                const { filename, path } = req.file
+                const fileData = await new fileSchema({
+                    filename: filename,
+                    path: path,
+                    uuid: uuid
+                });
+                await fileData.save(function (err, data) {
+                    if (err) throw err;
+                });
+                res.json({
+                    fileEndpoint: `${req.header('protocol') ? req.header('protocol') : ''}${req.header('Host')}/uploadfile/${uuid}`
+                })
+            }
+        } catch (error) {
+            res.status(510).send('something went worng')
         }
+
     }
 }
